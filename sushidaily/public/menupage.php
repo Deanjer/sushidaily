cf831e55b9889a9675464c934801d63d1fbb3cfa
Deanjer <220509@student.glu.nl>
2024-04-19 11:20:59 +0200

    frontpage-static-v1

--- sushidaily/public/menupage.php
+++ sushidaily/public/menupage.php
@@ -36,13 +36,87 @@
                     </svg>
                 </div>
                 <div class="menu">
+                    <!-- kaart 1 -->
                     <div class="menu-item">
                         <div class="product-placeholder-img"></div>
                         <!-- <img class="product-image" src="" alt=""> -->
-                        <h2>Title</h2>
-                        <p class="lato-regular">Description</p>
-                        <div class="feature">Feature</div>
+                        <p class="menu-title">Title</p>
+                        <p class="lato-regular menu-description">Description</p>
+                        <div class="feature lato-regular">Feature</div>
+                        <div class="lower-container">
+                            <p class="lato-regular price">€7,99</p>
+                            <div class="add-button">
+                                <svg width="16" height="16" viewBox="0 0 16 16" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M15 7H9V1C9 0.734784 8.89464 0.48043 8.70711 0.292893C8.51957 0.105357 8.26522 0 8 0C7.73478 0 7.48043 0.105357 7.29289 0.292893C7.10536 0.48043 7 0.734784 7 1V7H1C0.734784 7 0.48043 7.10536 0.292893 7.29289C0.105357 7.48043 0 7.73478 0 8C0 8.26522 0.105357 8.51957 0.292893 8.70711C0.48043 8.89464 0.734784 9 1 9H7V15C7 15.2652 7.10536 15.5196 7.29289 15.7071C7.48043 15.8946 7.73478 16 8 16C8.26522 16 8.51957 15.8946 8.70711 15.7071C8.89464 15.5196 9 15.2652 9 15V9H15C15.2652 9 15.5196 8.89464 15.7071 8.70711C15.8946 8.51957 16 8.26522 16 8C16 7.73478 15.8946 7.48043 15.7071 7.29289C15.5196 7.10536 15.2652 7 15 7Z"
+                                        fill="white" />
+                                </svg>
+
+                            </div>
+                        </div>
                     </div>
+                    <!-- kaart 2 -->
+                    <div class="menu-item">
+                        <div class="product-placeholder-img"></div>
+                        <!-- <img class="product-image" src="" alt=""> -->
+                        <p class="menu-title">Title</p>
+                        <p class="lato-regular menu-description">Description</p>
+                        <div class="feature lato-regular">Feature</div>
+                        <div class="lower-container">
+                            <p class="lato-regular price">€7,99</p>
+                            <div class="add-button">
+                                <svg width="16" height="16" viewBox="0 0 16 16" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M15 7H9V1C9 0.734784 8.89464 0.48043 8.70711 0.292893C8.51957 0.105357 8.26522 0 8 0C7.73478 0 7.48043 0.105357 7.29289 0.292893C7.10536 0.48043 7 0.734784 7 1V7H1C0.734784 7 0.48043 7.10536 0.292893 7.29289C0.105357 7.48043 0 7.73478 0 8C0 8.26522 0.105357 8.51957 0.292893 8.70711C0.48043 8.89464 0.734784 9 1 9H7V15C7 15.2652 7.10536 15.5196 7.29289 15.7071C7.48043 15.8946 7.73478 16 8 16C8.26522 16 8.51957 15.8946 8.70711 15.7071C8.89464 15.5196 9 15.2652 9 15V9H15C15.2652 9 15.5196 8.89464 15.7071 8.70711C15.8946 8.51957 16 8.26522 16 8C16 7.73478 15.8946 7.48043 15.7071 7.29289C15.5196 7.10536 15.2652 7 15 7Z"
+                                        fill="white" />
+                                </svg>
+
+                            </div>
+                        </div>
+                    </div>
+                    <!-- kaart 3 -->
+                    <div class="menu-item">
+                        <div class="product-placeholder-img"></div>
+                        <!-- <img class="product-image" src="" alt=""> -->
+                        <p class="menu-title">Title</p>
+                        <p class="lato-regular menu-description">Description</p>
+                        <div class="feature lato-regular">Feature</div>
+                        <div class="lower-container">
+                            <p class="lato-regular price">€7,99</p>
+                            <div class="add-button">
+                                <svg width="16" height="16" viewBox="0 0 16 16" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M15 7H9V1C9 0.734784 8.89464 0.48043 8.70711 0.292893C8.51957 0.105357 8.26522 0 8 0C7.73478 0 7.48043 0.105357 7.29289 0.292893C7.10536 0.48043 7 0.734784 7 1V7H1C0.734784 7 0.48043 7.10536 0.292893 7.29289C0.105357 7.48043 0 7.73478 0 8C0 8.26522 0.105357 8.51957 0.292893 8.70711C0.48043 8.89464 0.734784 9 1 9H7V15C7 15.2652 7.10536 15.5196 7.29289 15.7071C7.48043 15.8946 7.73478 16 8 16C8.26522 16 8.51957 15.8946 8.70711 15.7071C8.89464 15.5196 9 15.2652 9 15V9H15C15.2652 9 15.5196 8.89464 15.7071 8.70711C15.8946 8.51957 16 8.26522 16 8C16 7.73478 15.8946 7.48043 15.7071 7.29289C15.5196 7.10536 15.2652 7 15 7Z"
+                                        fill="white" />
+                                </svg>
+
+                            </div>
+                        </div>
+                    </div>
+                    <!-- kaart 4 -->
+                    <div class="menu-item">
+                        <div class="product-placeholder-img"></div>
+                        <!-- <img class="product-image" src="" alt=""> -->
+                        <p class="menu-title">Title</p>
+                        <p class="lato-regular menu-description">Description</p>
+                        <div class="feature lato-regular">Feature</div>
+                        <div class="lower-container">
+                            <p class="lato-regular price">€7,99</p>
+                            <div class="add-button">
+                                <svg width="16" height="16" viewBox="0 0 16 16" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M15 7H9V1C9 0.734784 8.89464 0.48043 8.70711 0.292893C8.51957 0.105357 8.26522 0 8 0C7.73478 0 7.48043 0.105357 7.29289 0.292893C7.10536 0.48043 7 0.734784 7 1V7H1C0.734784 7 0.48043 7.10536 0.292893 7.29289C0.105357 7.48043 0 7.73478 0 8C0 8.26522 0.105357 8.51957 0.292893 8.70711C0.48043 8.89464 0.734784 9 1 9H7V15C7 15.2652 7.10536 15.5196 7.29289 15.7071C7.48043 15.8946 7.73478 16 8 16C8.26522 16 8.51957 15.8946 8.70711 15.7071C8.89464 15.5196 9 15.2652 9 15V9H15C15.2652 9 15.5196 8.89464 15.7071 8.70711C15.8946 8.51957 16 8.26522 16 8C16 7.73478 15.8946 7.48043 15.7071 7.29289C15.5196 7.10536 15.2652 7 15 7Z"
+                                        fill="white" />
+                                </svg>
+
+                            </div>
+                        </div>
+                    </div>
+                    <!-- einde -->
                 </div>
             </div>
         </div>
