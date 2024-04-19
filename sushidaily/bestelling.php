7646cba5dc368be59238630ba3cfed1126cbcb85
Neochees <113420296+Neochees@users.noreply.github.com>
2024-04-19 12:10:46 +0200

    1 bestelling bijna af

--- sushidaily/bestelling.php
+++ sushidaily/bestelling.php
@@ -15,14 +15,24 @@
     <div class="center">
         <h1>Bestelling</h1>
         <div class="bestelling">
-            <div class="img">
-                
+            <div class="order">
+                <div class="image">
+                    
+                </div>
             </div>
             <div class="info">
-
+                <h2>Title</h2>
+                <div class="feature">
+                    feature
+                </div>
+                <div class="omschrijving">
+                    <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus aliquam magni commodi iure numquam maxime nulla
+                    </p>
+                </div>
             </div>
             <div class="prijs">
-
+                <h2>$7,99</h2>
             </div>
         </div>
     </div>
