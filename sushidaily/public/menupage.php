a383f37e977621ab4c913d93d2f310c6489f68a4
Deanjer <220509@student.glu.nl>
2024-04-19 09:06:00 +0200

    sidebarheader

---
+++ sushidaily/public/menupage.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./../assets/css/menupage.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container">
+        <header>
+            <img src="./../assets/img/logo-secondary-blue.svg" alt="" style="max-width: 360px">
+        </header>
+        <div class="center">
+            <div class="sidebar">
+                <ul>
+                    <li>Sushi</li>
+                    <li>Warme gerechten</li>
+                    <li>Pokébowls</li>
+                    <li>Dranken</li>
+                </ul>
+                <div class="exit">
+                    <p class="lato-bold">Stop bestelling</p>
+                </div>
+            </div>
+            <div class="menu-container">
+                <div class="menu-header">
+                    <h2 class="">Warme gerechten</h2>
+                    <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"
+                        class="shopping-cart">
+                        <path
+                            d="M280-80q-33 0-56.5-23.5T200-160q0-33 23.5-56.5T280-240q33 0 56.5 23.5T360-160q0 33-23.5 56.5T280-80Zm400 0q-33 0-56.5-23.5T600-160q0-33 23.5-56.5T680-240q33 0 56.5 23.5T760-160q0 33-23.5 56.5T680-80ZM246-720l96 200h280l110-200H246Zm-38-80h590q23 0 35 20.5t1 41.5L692-482q-11 20-29.5 31T622-440H324l-44 80h480v80H280q-45 0-68-39.5t-2-78.5l54-98-144-304H40v-80h130l38 80Zm134 280h280-280Z"
+                            fill="#001E42" />
+                    </svg>
+                </div>
+                <div class="menu">
+                    <div class="menu-item">
+                        <div class="product-placeholder-img"></div>
+                        <!-- <img class="product-image" src="" alt=""> -->
+                        <h2>Title</h2>
+                        <p class="lato-regular">Description</p>
+                        <div class="feature">Feature</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
