20ffa794dce2d2ae8433f22e04a17a35b4297d67
Neochees <113420296+Neochees@users.noreply.github.com>
2024-04-22 10:47:35 +0200

    bestellingen verder afgemaakt nu aleen de functie's en betere kleuren

--- sushidaily/bestelling.php
+++ sushidaily/bestelling.php
@@ -17,24 +17,63 @@
         <div class="bestelling">
             <div class="order">
                 <div class="image">
-                    
+
                 </div>
             </div>
             <div class="info">
-                <h2>Title</h2>
+                <h1>Title</h1>
+                <div class="categorie">
+                    <div class="feature">
+                        feature
+                    </div>
+                    <div class="feature">
+                        feature
+                    </div>
+                </div>
+                <div class="omschrijving">
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus aliquam magni commodi iure numquam maxime nulla
+                    </p>
+                </div>
+            </div>
+            <div class="prijs">
+                <h1>$7,99</h1>
+                <div class="hoeveelheid">
+                    <div class="min"><h2>-</h2></div>
+                    <div class="aantal"><h2>1</h2></div>
+                    <div class="plus"><h2>+</h2></div>
+                </div>
+            </div>
+        </div>
+        <hr class="rounded">
+        <div class="bestelling">
+            <div class="order">
+                <div class="image">
+
+                </div>
+            </div>
+            <div class="info">
+                <h1>Title</h1>
                 <div class="feature">
                     feature
                 </div>
                 <div class="omschrijving">
                     <p>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus aliquam magni commodi iure numquam maxime nulla
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus aliquam magni commodi iure numquam maxime nulla
                     </p>
                 </div>
             </div>
             <div class="prijs">
-                <h2>$7,99</h2>
+                <h1>$7,99</h1>
+                <div class="hoeveelheid">
+                    <div class="min"><h2>-</h2></div>
+                    <div class="aantal"><h2>1</h2></div>
+                    <div class="plus"><h2>+</h2></div>
+                </div>
             </div>
         </div>
+        <hr class="rounded">
     </div>
 </body>
+
 </html>
