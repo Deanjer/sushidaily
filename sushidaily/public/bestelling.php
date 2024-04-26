9cfb14cd1dd0f648df40d1d68b40a581b60057d1
Deanjer <220509@student.glu.nl>
2024-04-26 10:54:46 +0200

    link-structure

--- sushidaily/public/bestelling.php
+++ sushidaily/public/bestelling.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bestelling</title>
-    <link rel="stylesheet" href="./assets/css/bestelling.css">
+    <link rel="stylesheet" href="./../assets/css/bestelling.css">
 </head>
 
 <body>
